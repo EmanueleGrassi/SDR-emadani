537ca438b8b9406ece2590f1ad7acb1bfa862875
emanuelegrassi <emanuele.grassi@live.com>
2015-05-25 09:52:54 +0200

    main

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) throws Exception
     {
 		Signal signal;
-        for(int i = 1; i<=4; i++) {
+        for(int i = 1; i<4; i++) {
             for(int j = 1; j <=4; j++) {
                 signal = new Signal("./sequenze/Sequenza_"+i+"/output_"+j+".dat");
                 if(j!=2) System.out.print(signal);
